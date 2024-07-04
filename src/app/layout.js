b8a0531f2925a50d63d00632959f6d08defcf101
TheDevBianchi <119900283+TheDevBianchi@users.noreply.github.com>
2024-07-04 00:46:27 -0400

    New: SpeedInsight Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Montserrat } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
@@ -6,12 +7,13 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 export const metadata = {
   title: 'Rafael Bianchi',
   description:
-    'Portafolio web de Rafael Bianchi, desarrollador web autodidacta, comprometido con la misión de nunca parar de aprender.',
+    'Web portfolio of Rafael Bianchi, self-taught web developer, committed to the mission of never stopping learning.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <SpeedInsights />
       <body className={montserrat.className}>
         <main className='bg-main min-h-screen flex justify-center my-0 mx-auto max-w-[1349px] overflow-x-hidden'>
           {children}
