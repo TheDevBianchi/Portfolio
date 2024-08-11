591729b90094f98a8e2a72ee77b94494719f014f
TheDevBianchi <119900283+TheDevBianchi@users.noreply.github.com>
2024-08-11 14:56:23 -0400

    New: Adding Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Montserrat } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from "@vercel/analytics/react"
 import './globals.css'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
@@ -13,10 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <Analytics />
       <SpeedInsights />
-      <body className={montserrat.className}>
+      <body className={ montserrat.className }>
         <main className='bg-main min-h-screen flex justify-center my-0 mx-auto max-w-[1349px] overflow-x-hidden'>
-          {children}
+          { children }
         </main>
       </body>
     </html>
